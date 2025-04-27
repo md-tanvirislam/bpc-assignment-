69682cfd0578bf8ddf5118b404d2de65275939fd
mi23aey <mi23aey@herts.ac.uk>
2025-04-27 01:15:29 +0100

    uploading treatment java class

---
+++ Treatment.java
@@ -0,0 +1,17 @@
+public class Treatment {
+    private String name;
+    private String expertise;
+
+    public Treatment(String name, String expertise) {
+        this.name = name;
+        this.expertise = expertise;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getExpertise() {
+        return expertise;
+    }
+}
