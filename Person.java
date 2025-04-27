b69a231daae2aabb8fa8275315fc3ca96758be9a
mi23aey <mi23aey@herts.ac.uk>
2025-04-27 01:09:37 +0100

    uploading person class file

---
+++ Person.java
@@ -0,0 +1,30 @@
+public abstract class Person {
+    private int id;
+    private String name;
+    private String address;
+    private String phone;
+
+    public Person(int id, String name, String address, String phone) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
